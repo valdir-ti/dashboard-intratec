0935c69eff132bb115b900841ed13dc16eeb2a35
Valdir Silva <valdir.ti@gmail.com>
2023-04-24 21:10:59 -0300

    feat: split dash on components and add breakpoints

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-zinc-400 p-4">
+        <h2>Footer</h2>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <aside className="bg-zinc-400 p-4">
+        <h2>Dashboard Intratec Navbar</h2>
+    </aside>
+  )
+}
+
+export default Navbar
