1d7767a7d35985408175d706599d245e0b49e600
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-04-28 14:30:46 -0300

    feat: add menu on side navigation

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,3 +1,5 @@
+import Menu from "./Menu";
+
 interface SideNavigationProps {
     mobilePosition: boolean;
     openMenu: boolean;
@@ -6,9 +8,13 @@ interface SideNavigationProps {
 const SideNavigation = ({ mobilePosition, openMenu }: SideNavigationProps) => {
 
     return mobilePosition && openMenu ? (
-      <aside className="absolute right-0 top-0 h-screen w-48 bg-gray-200">
-        <div className='mt-4 flex justify-center'>
-            Sidenavigation
+      <aside className="absolute right-0 top-0 h-screen w-56 bg-gray-300">
+        <div className='mt-4 flex justify-center items-center'>
+          <h3 className='ml-2 font-bold mt-2'>Intratec Tecnologia</h3>
+        </div>
+        <span className="border-b-[1px] border-white pl-48 ml-4"></span>
+        <div className="ml-2 mt-2">
+          <Menu isOpen={true}/>
         </div>
       </aside>
     ) : null;
